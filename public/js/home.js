2f77dfd2a4e1462d5a642d5d497d26a3361d5de4
Chester Corpuz <chestercorpuzz@gmail.com>
2019-03-15 00:04:11 +0400

    Home page displays current user's polls

--- public/js/home.js
+++ public/js/home.js
@@ -4,4 +4,73 @@ function logout() {
   window.location.replace("/");
 }
 
-function getPolls() {}
+function getPolls() {
+  //  Fetch polls
+  var get_polls = new Request("api/polls/", {
+    method: "GET"
+  });
+
+  fetch(get_polls)
+    .then(res => {
+      if (!res.ok) {
+        return res.status(404).json({ error: "Polls not found." });
+      } else {
+        return res.json();
+      }
+    })
+    .then(allpolls => {
+      allpolls.forEach(poll => {
+        if (localStorage.getItem("id") === null) {
+          setTimeout(getPolls, 1000);
+        }
+        if (poll.user === localStorage.getItem("id")) {
+          var new_poll = document.createElement("fieldset");
+          new_poll.class = "poll-bound";
+          new_poll.innerHTML = `<legend id="poll-legend">\
+              ${poll.question} -\
+              <strong>${localStorage.getItem("name")}</strong>\
+            </legend>\
+            <form action="" id="${poll._id}" name="form1" method="POST">\
+              <label>\
+                <input type="radio" name="Poll" id="${poll.options[0]._id}" />\
+                ${poll.options[0].text}
+              </label>\
+              <label>\
+                <input type="radio" name="Poll" id="${poll.options[1]._id}" />\
+                ${poll.options[1].text}
+              </label>`;
+
+          if (poll.options[2].text !== undefined) {
+            new_poll.innerHTML += `<label>\
+                <input type="radio" name="Poll" id="${poll.options[2]._id}" />\
+                ${poll.options[2].text}
+              </label>`;
+          }
+
+          if (poll.options[3].text !== undefined) {
+            new_poll.innerHTML += `<label>\
+                <input type="radio" name="Poll" id="${poll.options[3]._id}" />\
+                ${poll.options[3].text}
+              </label>`;
+          }
+
+          new_poll.innerHTML += `<input type="submit" name="submit" id="submit" value="Vote" />\
+          <button\
+            type="button"\
+            name="button"\
+            onclick="document.getElementById(\'view-results\').style.display=\'block\'"\
+          >\
+            View results\
+          </button>\
+          <input type="hidden" name="id" value="form1" />\
+          <input type="hidden" name="MM_insert" value="form1" />\
+        </form>`;
+
+          document.getElementById("polls_container").appendChild(new_poll);
+        }
+      });
+    });
+  //  Print polls
+}
+
+getPolls();
