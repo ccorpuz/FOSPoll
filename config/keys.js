af5986fdc33a7f0fd8f1e18df245fa9820046d76
Chester Corpuz <chestercorpuzz@gmail.com>
2019-03-22 20:23:21 +0400

    Hidden Keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://admin:admin123@ds157064.mlab.com:57064/fospoll",
-  secret: "masterFahad"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
